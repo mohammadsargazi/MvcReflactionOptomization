1d30a51bfe5a6bfbcd10873683f5bde91e450fa1
mohammadsargazi <m.sargazidev@gmail.com>
2023-07-02 13:04:10 +0330

    Add UserController class inheriting from GenericController

---
+++ OptimizeTranditionalReflaction/Controllers/UserController.cs
@@ -0,0 +1,10 @@
+﻿using CommonMvc.Entities;
+using CommonMvc;
+
+namespace OptimizeTranditionalReflaction.Controllers;
+
+public class UserController : GenericController<User, GenericMvcDbContext>
+{
+    public UserController(GenericMvcDbContext context, IConfiguration config, IWebHostEnvironment webHostEnvironment)
+        : base(context, config, webHostEnvironment) { }
+}
