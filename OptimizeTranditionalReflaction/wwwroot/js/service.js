d1ca6026aabda958c3fadce1bf6a418c482fed0d
mohammadsargazi <m.sargazidev@gmail.com>
2023-07-02 12:49:01 +0330

    Add MVC architecture for optimized reflection in project

---
+++ OptimizeTranditionalReflaction/wwwroot/js/service.js
@@ -0,0 +1,39 @@
+﻿function post(controller, action, data, callBackFun) {
+    $.ajax({
+        url: '/' + controller + '/' + action,
+        type: 'Post',
+        dataType: 'json',
+        beforeSend: function () {
+            loadingBoxStart();
+        },
+        data: JSON.stringify(data),
+        success: function (item) {
+            callBackFun(item);
+        },
+        complete: function () {
+            loadingBoxStop();
+        },
+    });
+}
+function postAndRedirect(controller, action, data,returnUrl) {
+    $.ajax({
+        url: '/' + controller + '/' + action,
+        type: 'Post',
+        dataType: 'json',
+        beforeSend: function () {
+            loadingBoxStart();
+        },
+        data: JSON.stringify(data),
+        success: function (item) {
+            if (!item.res) {
+                $.growl.error({ message: data.message });
+            }
+            else {
+                location.href = returnUrl;
+            }
+        },
+        complete: function () {
+            loadingBoxStop();
+        },
+    });
+}
---
+++ OptimizeTranditionalReflaction/wwwroot/login/js/site.js
@@ -0,0 +1,55 @@
+﻿
+var validate = true;
+function validateError() {
+    validate = true;
+    $('input').each(function () {
+        if ($.inArray(@Convert.ToDecimal(RolesEnum.Admin), roleIds) !== -1) {
+        if ($(this).val() == "") {
+            var id = $(this).attr('id');
+            var txt = $('label[for="' + id + '"]').text();
+            if (txt != "") {
+                txt = txt + " را وارد نمایید."
+                $.growl.warning({ message: txt, title: "کاربر گرامی" });
+                validate = false;
+            }
+        }
+    }
+    if ($.inArray(@Convert.ToDecimal(RolesEnum.Admin), roleIds) === -1 && !$(this).closest('div').hasClass('show-to-admin')) {
+        if ($(this).val() == "") {
+            var id = $(this).attr('id');
+            var txt = $('label[for="' + id + '"]').text();
+            if (txt != "") {
+                txt = txt + " را وارد نمایید."
+                $.growl.warning({ message: txt, title: "کاربر گرامی" });
+                validate = false;
+            }
+        }
+    }
+
+
+});
+$('select').each(function () {
+    if ($.inArray(@Convert.ToDecimal(RolesEnum.Admin), roleIds) !== -1) {
+    if ($(this).val() == "" || $(this).val() == 0) {
+        var id = $(this).attr('id');
+        var txt = $('label[for="' + id + '"]').text();
+        if (txt != "") {
+            txt = txt + " را وارد نمایید."
+            $.growl.warning({ message: txt, title: "کاربر گرامی" });
+            validate = false;
+        }
+    }
+}
+if ($.inArray(@Convert.ToDecimal(RolesEnum.Admin), roleIds) === -1 && !$(this).closest('div').hasClass('show-to-admin')) {
+    if ($(this).val() == "" || $(this).val() == 0) {
+        var id = $(this).attr('id');
+        var txt = $('label[for="' + id + '"]').text();
+        if (txt != "") {
+            txt = txt + " را وارد نمایید."
+            $.growl.warning({ message: txt, title: "کاربر گرامی" });
+            validate = false;
+        }
+    }
+}
+        })
+    }
